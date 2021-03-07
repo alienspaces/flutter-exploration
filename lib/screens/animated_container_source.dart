a55a23a5c1b5fc0a40807d0323ab73604f9870ba
alienspaces <alienspaces@gmail.com>
2021-03-07 15:55:09 +1100

    Add end drawer for code display

--- lib/screens/animated_container_source.dart
+++ lib/screens/animated_container_source.dart
@@ -26,39 +26,63 @@ class _AnimatedContainerAppState extends State<AnimatedContainerScreen> {
   Color _color = Colors.green;
   BorderRadiusGeometry _borderRadius = BorderRadius.circular(8);
 
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+
+  void _openEndDrawer() {
+    _scaffoldKey.currentState.openEndDrawer();
+  }
+
+  void _closeEndDrawer() {
+    Navigator.of(context).pop();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(\'Animated Container\'),
-      ),
-      body: Container(
-        child: Row(
-          children: <Widget>[
-            Expanded(
-              flex: 1,
-              child: Container(
-                // Creates a container that animates its parameters implicitly.
-                child: AnimatedContainer(
-                  width: _width,
-                  height: _height,
-                  decoration: BoxDecoration(
-                    color: _color,
-                    borderRadius: _borderRadius,
-                  ),
-                  duration: Duration(seconds: 1),
-                  curve: Curves.fastOutSlowIn,
-                ),
-              ),
-            ),
-            Expanded(
-              flex: 1,
-              child: Container(
-                child: SourceWidget(),
-              ),
-            ),
-          ],
+      key: _scaffoldKey,
+      appBar: AppBar(title: Text(\'Animated Container\'), actions: <Widget>[
+        // Show source code
+        Container(
+          width: 100,
+          height: 50,
+          child: ElevatedButton(
+            onPressed: _openEndDrawer,
+            child: Text(\'Code\'),
+          ),
         ),
+      ]),
+      body: Container(
+        alignment: Alignment.center,
+        // Creates a container that animates its parameters implicitly.
+        child: AnimatedContainer(
+          width: _width,
+          height: _height,
+          decoration: BoxDecoration(
+            color: _color,
+            borderRadius: _borderRadius,
+          ),
+          duration: Duration(seconds: 1),
+          curve: Curves.fastOutSlowIn,
+        ),
+      ),
+      // Source Code
+      endDrawer: Stack(
+        alignment: AlignmentDirectional.topEnd,
+        children: <Widget>[
+          Scrollbar(
+            child: SingleChildScrollView(
+              child: SourceWidget(),
+            ),
+          ),
+          Container(
+            width: 100,
+            height: 50,
+            child: ElevatedButton(
+              onPressed: _closeEndDrawer,
+              child: const Text(\'Close Drawer\'),
+            ),
+          ),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.play_arrow),
