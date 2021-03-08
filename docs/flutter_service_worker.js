e03ea99e8cd16d897ed43c8019804f8f23dad0a5
alienspaces <alienspaces@gmail.com>
2021-03-08 19:44:08 +1100

    Updated web build

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -12,10 +12,10 @@ const RESOURCES = {
 "assets/NOTICES": "0f8ef577d1a69c71f77ba35de5226a2e",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/fonts/MaterialIcons-Regular.otf": "1288c9e28052e028aba623321f7826ac",
-"main.dart.js": "07d78076a4849289c2ec9cc130c94f01",
+"main.dart.js": "57e5ea133752d1adca9492e68dd2e4cc",
 "manifest.json": "190711d25f577894a01d6e4cc37c8249",
-"index.html": "e955cbb1044de9fc8cee8e7faf2d35a8",
-"/": "e955cbb1044de9fc8cee8e7faf2d35a8",
+"index.html": "7abb0963f2ed34fb1c8b95b5fae733b4",
+"/": "7abb0963f2ed34fb1c8b95b5fae733b4",
 "version.json": "cbc504d1ea811921213ee8d0c9c6e1e8"
 };
 
